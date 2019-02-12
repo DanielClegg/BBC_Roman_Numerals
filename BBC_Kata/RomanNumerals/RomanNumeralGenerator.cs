3854c79e5689aaca93378bfe09a62a24753a836a
Daniel Clegg <dc@danielclegg.com>
2019-02-12 14:11:29 +0000

    Some tests passing

--- BBC_Kata/RomanNumerals/RomanNumeralGenerator.cs
+++ BBC_Kata/RomanNumerals/RomanNumeralGenerator.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace RomanNumerals
 {
     public class RomanNumeralGenerator : IRomanNumeralGenerator
     {
+
+        Dictionary<string, int> _romanNumeralValues = new Dictionary<string, int>() {
+            {"I", 1}, 
+            {"V",5},
+            {"X", 10}, 
+            {"L", 50}, 
+            {"C", 100}, 
+            {"D",500}};
+        
         public string Generate(int number)
         {
-            throw new NotImplementedException();
+            foreach (var numeral in _romanNumeralValues)
+            {
+                if (number == numeral.Value)
+                    return numeral.Key;
+            }
+
+            return null;
         }
     }
 }
